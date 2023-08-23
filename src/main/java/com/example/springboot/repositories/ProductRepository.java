55046554162454d11a71304ed33fc431f7ad9b16
Yago Alexandre <goltarayago@gmail.com>
2023-08-22 22:58:35 -0300

    feat: complete crud

---
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.springboot.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springboot.models.ProductModel;
+
+public interface ProductRepository extends JpaRepository<ProductModel, UUID>{
+
+}
